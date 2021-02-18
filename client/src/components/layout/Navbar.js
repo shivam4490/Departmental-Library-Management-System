4bc64bfa95e61e65106967ca95e9b06c563c2e8c
shivam4490 <csg9152@gmail.com>
2021-02-18 12:10:28 +0530

    'auth'

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <>
+        <nav className='navbar bg-light justify-content-center'>
+          <Link className='navbar-brand text-dark font-weight-bold' to='/'>
+            Library App
+          </Link>
+        </nav>
+      </>
+    )
+  }
+}
+
+export default Navbar
